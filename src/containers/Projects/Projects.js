75a4481e3da739e43de2e5dae4bc29e453e560a7
akcshing <30954555+akcshing@users.noreply.github.com>
2019-06-06 22:13:15 +0100

    Fix projects page responsive styling

--- src/containers/Projects/Projects.js
+++ src/containers/Projects/Projects.js
@@ -7,10 +7,12 @@ const Projects = (props) => (
 		<h1>PROJECTS</h1>
 		<div className="project-container">
 			<img alt="transactions" src={transactions}></img>
-			<h2>Spending Tracker</h2>
+			<div className="project-title">
+				<h2>Spending Tracker</h2>
+				<h3>A Full-Stack Web App.</h3>
+			</div>
 			<div className="project-info">
 				<div className="project-info-p">
-					<h3>A Full-Stack Web App.</h3>
 					<p>This was my first project at CodeClan. A solo project that lasted a week with a presentation at the end. We were allowed to choose from a number of briefs which specified what was required, but the fundamental spec was to create an app with CRUD functionality and RESTful routes. Another requirement was to make use of SQL table relationships.</p>
 					<p>This app allows the user to enter monetary transactions they wish to log. The transactions require a 'merchant' and a 'tag', all of which the user can create, edit and delete. The app makes use of SQL table relations to display relevant transaction data for each class. During this solo project, I practiced planning from the UX perspective, creating user stories and journeys.</p>
 				</div>
